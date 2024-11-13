983f437db9ab774db3d20529c8fb897f23e84f34
khalili <m.khalili7310@gmail.com>
2024-11-13 15:25:41 +0330

    Advanced techniques - Code structuring for bigger projects

--- src/026-Code-Structuring-for-bigger-projects/Experience/Experience.js
+++ src/026-Code-Structuring-for-bigger-projects/Experience/Experience.js
@@ -5,6 +5,8 @@ import Camera from "./Camera";
 import Renderer from "./Renderer";
 import World from "./World/World";
 import Resources from "./Utils/Resources";
+import Debug from "./Utils/Debug";
+import sources from "./sources";
 
 let instance = null;
 
@@ -22,10 +24,11 @@ export default class Experience {
     this.canvas = canvas;
 
     // Setup
+    this.debug = new Debug();
     this.sizes = new Sizes();
     this.time = new Time();
     this.scene = new THREE.Scene();
-    this.resources = new Resources();
+    this.resources = new Resources(sources);
     this.camera = new Camera();
     this.renderer = new Renderer();
     this.world = new World();
@@ -48,6 +51,34 @@ export default class Experience {
 
   update() {
     this.camera.update();
+    this.world.update();
     this.renderer.update();
+  }
+
+  destroy() {
+    this.sizes.off("resize");
+    this.time.off("tick");
+
+    // Traverse the whole scene
+    this.scene.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        child.geometry.dispose();
+
+        for (const key in child.material) {
+          const value = child.material[key];
+
+          if (value && typeof value.dispose === "function") {
+            value.dispose();
+          }
+        }
+      }
+    });
+
+    this.camera.controls.dispose();
+    this.renderer.instance.dispose();
+
+    if (this.debug.active) {
+      this.debug.ui.destroy();
+    }
   }
 }
