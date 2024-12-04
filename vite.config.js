7cbaaa8a33ba2d17083b868dc77d930c740f3a40
khalili <m.khalili7310@gmail.com>
2024-12-04 14:56:57 +0330

    Shaders - 041-GPGPU flow field

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import glsl from "vite-plugin-glsl";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    sourcemap: true,
+    target: "esnext",
+  },
   plugins: [glsl()],
 });
